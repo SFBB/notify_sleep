2a8901a5154e8641ebd8b8696b36394aa3c31c47
SFBB <anzhe990206@outlook.com>
2026-09-03 00:24:26 +0800

    fix: Make osd applying more robust.

--- src/bin/x11.rs
+++ src/bin/x11.rs
@@ -25,11 +25,11 @@ fn get_screen_resolution() -> (f32, f32) {
     (1920.0, 1080.0)
 }
 
-fn apply_osd_properties_to_x11_window(win_id: u32) -> Result<(), Box<dyn std::error::Error>> {
-    let (conn, screen_num) = x11rb::connect(None)?;
-    let screen = &conn.setup().roots[screen_num];
-    let root = screen.root;
-
+fn apply_osd_properties(
+    conn: &impl Connection,
+    root: u32,
+    win_id: u32,
+) -> Result<(), Box<dyn std::error::Error>> {
     let net_wm_type = conn
         .intern_atom(false, b"_NET_WM_WINDOW_TYPE")?
         .reply()?
@@ -129,6 +129,58 @@ fn apply_osd_properties_to_x11_window(win_id: u32) -> Result<(), Box<dyn std::er
     Ok(())
 }
 
+fn is_osd_confirmed(conn: &impl Connection, win_id: u32) -> bool {
+    let Ok(net_wm_state) = conn.intern_atom(false, b"_NET_WM_STATE") else {
+        return false;
+    };
+    let Ok(net_wm_state) = net_wm_state.reply() else {
+        return false;
+    };
+    let Ok(skip_taskbar) = conn.intern_atom(false, b"_NET_WM_STATE_SKIP_TASKBAR") else {
+        return false;
+    };
+    let Ok(skip_taskbar) = skip_taskbar.reply() else {
+        return false;
+    };
+
+    let Ok(prop) = conn.get_property(
+        false,
+        win_id,
+        net_wm_state.atom,
+        AtomEnum::ATOM,
+        0,
+        32,
+    ) else {
+        return false;
+    };
+    let Ok(reply) = prop.reply() else {
+        return false;
+    };
+
+    reply.value.chunks_exact(4).any(|chunk| {
+        u32::from_ne_bytes(chunk.try_into().unwrap()) == skip_taskbar.atom
+    })
+}
+
+fn ensure_osd_properties(win_id: u32) {
+    std::thread::spawn(move || {
+        let Ok((conn, screen_num)) = x11rb::connect(None) else {
+            return;
+        };
+        let root = conn.setup().roots[screen_num].root;
+
+        for _ in 0..50 {
+            let _ = apply_osd_properties(&conn, root, win_id);
+
+            if is_osd_confirmed(&conn, win_id) {
+                break;
+            }
+
+            std::thread::sleep(Duration::from_millis(40));
+        }
+    });
+}
+
 struct SleepOdsApp {
     target_time: Instant,
     state_machine: OsdStateMachine,
@@ -154,8 +206,7 @@ impl eframe::App for SleepOdsApp {
         if self.first_frame {
             self.first_frame = false;
             if let Some(id) = self.win_id {
-                println!("first frame-window id: {}!", id);
-                let _ = apply_osd_properties_to_x11_window(id);
+                ensure_osd_properties(id);
             }
         }
 
@@ -179,6 +230,9 @@ impl eframe::App for SleepOdsApp {
             ctx.send_viewport_cmd(egui::ViewportCommand::InnerSize(corner.size()));
             ctx.send_viewport_cmd(egui::ViewportCommand::OuterPosition(corner.min));
             self.is_window_shrunk = true;
+            if let Some(id) = self.win_id {
+                ensure_osd_properties(id);
+            }
         }
 
         let remaining = if self.target_time > now {
@@ -248,14 +302,12 @@ fn main() -> eframe::Result<()> {
                 match handle.as_raw() {
                     RawWindowHandle::Xlib(h) => {
                         let id = h.window as u32;
-                        println!("xlib-window id: {}!", id);
-                        let _ = apply_osd_properties_to_x11_window(id);
+                        ensure_osd_properties(id);
                         win_id = Some(id);
                     }
                     RawWindowHandle::Xcb(h) => {
                         let id = h.window.get();
-                        println!("xcb-window id: {}!", id);
-                        let _ = apply_osd_properties_to_x11_window(id);
+                        ensure_osd_properties(id);
                         win_id = Some(id);
                     }
                     _ => {}
